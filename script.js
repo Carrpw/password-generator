bdbcb43dab758388b2c6ec212f133fa9e2f9187d
Paul Carr <pwcarr15@gmail.com>
2020-11-02 18:06:56 -0500

    Sstarted generatePasword function for length.

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var special = ["!", "@", "#", "$", "%", "^", "&", "*", "~", "=", "+", "?", "<", ">"]
-  
+var upper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var lower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var numeric = ['0','1','2','3','4','5','6','7','8','9'];
+var special = ['!','@','#','$','%','^','&','*','(',')','-','+','=','~','<','>','?'];
+
+function generatePassword() {
+
+  var length = parseInt(prompt("How many characters (8-128) would you like your password to contain?"));
+
+  if (length < 8) {
+    alert('The password must be at least 8 characters!');
+    return;
+  }
+
+  if (length > 128) {
+    alert('The password must be less than 128 characters!');
+    return;
+  }
+
  
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
